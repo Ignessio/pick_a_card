151906ae66e5a4893a001062585f8f805922bff2
valentin_zet <xerrox@yandex.ru>
2021-09-29 12:02:19 +0300

    first commit. pick a card file added

---
+++ pick_a_card.rb
@@ -0,0 +1,5 @@
+values = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace']
+suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+
+# Выведем произвольную карту, выбрав по одному элементу из каждого массива
+puts "#{values.sample} #{suits.sample}"
