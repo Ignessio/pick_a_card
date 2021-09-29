761b6ba3983b74d80bfbe5c0f6a7b8c1d2c4403a
valentin_zet <xerrox@yandex.ru>
2021-09-29 12:09:57 +0300

    suits names changed to symbols

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,5 @@
 values = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+suits = ['♦', '♠', '♥ ', '♣']
 
 # Выведем произвольную карту, выбрав по одному элементу из каждого массива
 puts "#{values.sample} #{suits.sample}"
